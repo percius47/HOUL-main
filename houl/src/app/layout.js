8f0360709d7dc7a55d67329b6e1b69b29ca33505
percius47 <167627168+skystartech@users.noreply.github.com>
2024-10-09 19:15:40 +0530

    fix: page title

--- houl/src/app/layout.js
+++ houl/src/app/layout.js
@@ -5,15 +5,15 @@ import TopBar from "./Components/Topbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "HOUL",
-  description: "Next-Gen Streaming Platform: 0% commission pay-per-use platform with free starter plan for 1 month.",
+  title: "Houl",
+  description:
+    "Next-Gen Streaming Platform: 0% commission pay-per-use platform with free starter plan for 1 month.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
       <body className={inter.className}>
-
         {/* <TopBar username={user.email} userId={user.uid} /> */}
         {children}
       </body>
